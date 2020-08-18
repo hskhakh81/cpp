91586f7a5270a6d0756fc3066689b479dcff4e31
Harpreet Khakh <hskhakh@yahoo.com>
2020-08-18 00:54:13 -0700

    Added content to function find_gpf() greatest_prime_factor_recursion.cpp, this version is not working correctly

--- greatest_prime_factor_recursion.cpp
+++ greatest_prime_factor_recursion.cpp
@@ -4,15 +4,15 @@
     Date        User        Comments
     ----------  ----------- ---------------------------------------
     08/17/20    Harpreet    1) Copied from file greatest_prime_factor.cpp
-                            2) Created function "int find_gpf(int num, int gpf)"
+                            2) Created function "int find_gpf(int num, int div, int gpf)"
                             3)
 */
 
 #include <iostream>
-#include <conio.h>
+
 using namespace std;
 
-int find_gpf(int , int);
+int find_gpf(int , int, int);
 
 int main(){
     int num;
@@ -23,51 +23,28 @@ int main(){
     //read input input num variable
     cin >> num;
 
-    //check if number if prime
-    for (int i=1; i <= num; i++) {  //outer loop
-        //check if number is divisible by i
-        if (num % i == 0) {
-            cout << i <<" x "<<num/i<<", ";
+    int gpf = find_gpf(num,2,num);
 
-            for (int j=2; j<=i/2;j++) {     //inner loop
-                //cout << ", check if " << i << " is prime ... ";
-                if (i % j == 0) {
-                    cout << " but " << i << " is not prime";
-                    cout << ", Greatest Prime factor still is " << largest_prime_factor;
-                    prime_flag = 0;
-                }
-            }
-            if (prime_flag) {
-                cout << " and " << i <<" is prime ";
-                if (largest_prime_factor < i) {
-                    cout << ", Greatest Prime factor now is " << i;
-                    largest_prime_factor = i;
-                } else {
-                    cout << ", Greatest Prime factor still is " << largest_prime_factor;
-                }
-            } //end prime flag condition
-            cout << "\n";
-        }   //end modulus condition
-
-        //reset prime_flag
-        prime_flag = 1;
-    } //end for loop
-
-    if (largest_prime_factor > 0) {
-        cout << "Greatest prime factor of " << num << " is " << largest_prime_factor << "\n";
-    }
-
-    getch();    //wait for user to press a key
+    cout << " GPF for " << num << " is " << gpf << endl;
+    getchar();    //wait for user to press a key
     return 0;
 }
 
-int find_gpf(int num, int gpf) {
+int find_gpf(int num, int div, int gpf) {
 
-    if (gpf <= num) {
-        //recursion
-        find_gpf(num,gpf+1);
-    } else {
+    if (gpf = 1) {
         //base case
+        return div;
+    } else {
+        //calcualte new values (i.e. make problem smaller as per recursion)
+        if (num % div == 0) {
+            gpf = num / div;
+            num = gpf;
+        } else {
+            div++;
+        }
+        //call self as per recursion (notice parameters values have changed with above calculations)
+        find_gpf(num, div, gpf);
 
     }
 }
